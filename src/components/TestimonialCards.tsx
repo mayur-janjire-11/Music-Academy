a818d08d30c6580377e95312233f6491a061ddf6
mayur-janjire-11 <mayurjanjire111@gmail.com>
2026-06-25 13:38:08 +0530

    add Background Grid

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { cn } from "@/utils/cn";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 const musicSchoolTestimonials = [
@@ -36,7 +37,14 @@ const musicSchoolTestimonials = [
 const TestimonialCards = () => {
   return (
         <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
-        <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
+
+        <div className={cn(
+          "absolute inset-0",
+          "[background-size:40px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        )}>
+          <h2 className="text-3xl font-bold text-center mt-20 mb-8 z-10">Hear our Harmony: Voices of success</h2>
         <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
             <div className="w-full max-w-6xl">
             <InfiniteMovingCards
@@ -45,6 +53,7 @@ const TestimonialCards = () => {
                 speed="normal"
       />
             </div>
+        </div>
         </div>
     </div>
   )
